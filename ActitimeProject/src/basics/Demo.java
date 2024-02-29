6e950add415dddcd2d85cff79ff679f4a592874c
QSP <akshay.python@gmail.com>
2024-02-29 13:01:11 +0530

    first commit

---
+++ ActitimeProject/src/basics/Demo.java
@@ -0,0 +1,10 @@
+package basics;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Demo {
+public static void main(String[] args) {
+	  EdgeDriver driver = new EdgeDriver();
+	driver.get("https://www.google.com");
+}
+}
