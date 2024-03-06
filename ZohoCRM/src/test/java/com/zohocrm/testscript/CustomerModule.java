5954ad5a6cbde4bf046d5d93bd03c5ef75f0ef33
QSP <akshay.python@gmail.com>
2024-03-06 17:51:31 +0530

    First commit

---
+++ ZohoCRM/src/test/java/com/zohocrm/testscript/CustomerModule.java
@@ -0,0 +1,11 @@
+package com.zohocrm.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+@Test
+public void testCreateCustomer() {
+	Reporter.log("CreateCustomer",true);
+}
+}
